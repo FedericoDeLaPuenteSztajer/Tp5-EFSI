9b1b908c23b8c9b0dfc312867bccfb4295a5348d
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-28 13:56:18 -0300

    api arreglada

--- src/modules/api.jsx
+++ src/modules/api.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 //API page: https://www.omdbapi.com/
 //API key: 21273b29
+//Example: https://www.omdbapi.com/?apikey=21273b29&t=Toy+Story
 
 const api = axios.create({
-    baseURL: 'http://www.omdbapi.com/?apikey=[21273b29]&'
+    baseURL: 'https://www.omdbapi.com/?apikey=21273b29'
 })
 
 export default api;
